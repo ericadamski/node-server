900db618682b41f498ffc0eb59682176db55881e
Tyler Fry <tyler.fry@mb3online.com>
2017-05-15 16:03:09 -0400

    Refactor to move response handling to the controller.

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,39 +1,61 @@
 import { Person } from '../models';
 import { Authenticate } from '../services';
+import { error } from '../utils';
 
 export default class PeopleController {
-    static index() {
-        return Person.fetchAll();
+    static async index(req, res) {
+        const people = await Person.fetchAll();
+        return res.status(200).json(people);
     }
 
-    static show(id) {
-        return Person.find(id);
-    }
-
-    static update(id, data) {
-        return Person.find(id)
-            .then(person => {
-                if (!person) return false
-
-                return person.save(data, { patch: true });
-            });
-    }
-
-    static create(data) {
-        const { password, ...model } = data;
-        if (password) {
-            return Authenticate.hash(password)
-                .then(password => {
-                    const person = new Person({ password, ...model });
-
-                    return person.save();
-                });
+    static async show(req, res) {
+        const { id } = req.params;
+        const person = await Person.find(id);
+        if (person) {
+            return res.status(200).json(person);
+        } else {
+            return res.status(404).json(error(`Person with ID ${id}, cannot be found.`));
         }
-
-        return Promise.reject();
     }
 
-    static destroy(id) {
+    static async update(res, req) {
+        const { id } = req.params;
+        const { body } = req.body;
+        const person = await Person.find(id);
+        
+        if (person) {
+            const response = await person.save(body, { patch: true });
+            if (response) return res.status(204).end();
+        } else {
+            return res.status(404).json(error(`Person with ID ${id}, cannot be found.`));
+        }
+    }
 
+    static async create(req, res) {
+        const { password, ...model } = req.body;
+        
+        if (password) {
+            const pass = await Authenticate.hash(password);
+            try {
+                const person = await new Person({ password: pass, ...model }).save();
+                return res.status(201).end();
+            } catch (e) {
+                return res.status(404).json(error(e.stack, e));
+            }
+        }
+        
+        return res.status(404).json(error(`Password is required`));
+    }
+
+    static async destroy(req, res) {
+        const { id } = req.params;
+        const person = await Person.find(req.params.id);
+        
+        if (person) {
+            const response = await person.destroy();
+            return res.status(204).end();
+        } else {
+            return res.status(404).json(error(`Person with ID ${id}, cannot be found.`))
+        }
     }
 }
--- routes/api/v1/people.js
+++ routes/api/v1/people.js
@@ -9,38 +9,12 @@ const form = Multer();
 const router = Router();
 
 router
-    .get('/', async (req, res) => {
-        const people = await PeopleController.index();
-        res.status(200).json(people);
-    })
-    .post('/', form.array(), async (req, res) => {
-        const person = await PeopleController.create(req.body);
-        res.status(201).end();
-    });
+    .get('/', PeopleController.index)
+    .post('/', form.array(), PeopleController.create);
 
 router
-    .get('/:id', async (req, res) => {
-        const { id } = req.params;
-
-        const person = await PeopleController.show(id);
-
-        if (!person) {
-            res.status(404).json(error(`Person with ID ${id}, cannot be found.`));
-        } else {
-            res.status(200).json(person);
-        }
-    })
-    .patch('/:id', form.array(), async (req, res) => {
-        const { body } = req;
-        const { id } = req.params;
-
-        const person = await PeopleController.update(id, body);
-
-        if (!person) {
-            res.status(404).json(error(`Person with ID ${id}, cannot be found.`));
-        } else {
-            res.status(204).end();
-        }
-    });
+    .get('/:id', PeopleController.show)
+    .patch('/:id', form.array(), PeopleController.update)
+    .delete('/:id', PeopleController.destroy);
 
 export default router;
